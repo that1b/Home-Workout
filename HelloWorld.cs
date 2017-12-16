36d21d87c3b98fca3f4e1992f9a05762f383b3c8
Tolga Hatib <31797377+that1b@users.noreply.github.com>
2017-12-16 02:01:18 +0200

    Create HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Tolga_Hello_World
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.ReadLine();
+		}
+	}
+}
